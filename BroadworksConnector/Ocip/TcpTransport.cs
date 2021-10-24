0412598133395a6eac4c853e763e888995706114
Chase Miller <chase@miller.sh>
2021-10-23 20:30:28 -0400

    Allow exceptions through on TcpTransport.SendAsync
    No longer catch all exceptions and wrap them in BadResponseException.

--- BroadworksConnector/Ocip/TcpTransport.cs
+++ BroadworksConnector/Ocip/TcpTransport.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net.Security;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -28,6 +27,8 @@ namespace BroadWorksConnector.Ocip
         private Stream _stream;
 
         private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+
+        private const string EndTag = "</BroadsoftDocument>\n";
 
         /// <summary>
         /// Constructor
@@ -67,13 +68,14 @@ namespace BroadWorksConnector.Ocip
 
             try
             {
-                // Connect to host
+                // Connect to host if this is the first time called
                 if (_tcpClient == null)
                 {
-
-                    _tcpClient = new TcpClient(_host, _port);
-                    _tcpClient.SendTimeout = _options.TcpSendTimeout;
-                    _tcpClient.ReceiveTimeout = _options.TcpReceiveTimeout;
+                    _tcpClient = new TcpClient(_host, _port)
+                    {
+                        SendTimeout = _options.TcpSendTimeout,
+                        ReceiveTimeout = _options.TcpReceiveTimeout
+                    };
 
                     if (_useSsl)
                     {
@@ -86,40 +88,33 @@ namespace BroadWorksConnector.Ocip
                     }
                 }
 
+                // Using the configured encoding, convert the XML request string into bytes
                 var requestData = _options.TcpRequestEncoding.GetBytes(request);
+
+                // Write all of the XML request to the socket
+                await _stream.WriteAsync(requestData, 0, requestData.Length, cancellationToken).ConfigureAwait(false);
 
                 var responseData = new List<byte>();
                 var responseBuffer = new byte[1024];
+                int bytesRead = -1;
 
-                await _stream.WriteAsync(requestData, 0, requestData.Length, cancellationToken).ConfigureAwait(false);
-
-                try
+                while (bytesRead != 0)
                 {
-                    int bytesRead = -1;
+                    // Read in some response bytes from the socket
+                    bytesRead = await _stream.ReadAsync(responseBuffer, 0, responseBuffer.Length, cancellationToken).ConfigureAwait(false);
 
-                    do
+                    // Append received bytes to full buffer
+                    responseData.AddRange(responseBuffer.Take(bytesRead));
+
+                    // Read the full response as a string using the encoding provided in the client options
+                    response = _options.TcpResponseEncoding.GetString(responseData.ToArray());
+
+                    // Once the response contains the ending tag, return it
+                    if (response.Contains(EndTag))
                     {
-                        // Clear out response buffer for each read so there's no hanging data from the previous read
-                        bytesRead = await _stream.ReadAsync(responseBuffer, 0, responseBuffer.Length, cancellationToken).ConfigureAwait(false);
-
-                        // Append buffer contents to full response
-                        responseData.AddRange(responseBuffer.Take(bytesRead));
-
-                        // Read the full response as a string using the encoding provided in the client options
-
-                        response = _options.TcpResponseEncoding.GetString(responseData.ToArray());
-
-                        // Once the response contains the ending tag, return it
-                        if (response.Contains("</BroadsoftDocument>\n"))
-                        {
-                            break;
-                        }
-                    } while (bytesRead != 0);
-                }
-                catch (Exception e) when (!(e is OperationCanceledException))   // Allow exceptions caused by cancellationToken to fall through 
-                {
-                    throw new BadResponseException("Unable to parse response", e);
-                }
+                        break;
+                    }
+                } 
             }
             finally
             {
